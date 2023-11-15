9cb92d77ed156ffc9e25a59b2eaf325d73726484
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-11-15 14:45:16 +0000

    Add project files.

---
+++ text-game/Character.cs
@@ -0,0 +1,21 @@
+﻿
+namespace text_game;
+
+internal class Character
+{
+    public string name;
+    public string role;
+
+    public int health;
+    public int attack;
+    public int defence;
+
+    public Character(string name, string role, int health, int attack, int defence)
+    {
+        this.name = name;
+        this.role = role;
+        this.health = health;
+        this.attack = attack;
+        this.defence = defence;
+    }
+}
---
+++ text-game/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace text_game;
+
+internal class Program
+{
+    static void Main()
+    {
+        Game();
+    }
+
+    static void Game() 
+    {
+        CreateCharacter();
+    }
+
+    static void CreateCharacter() 
+    {
+        
+    }
+}
