f60b1dcb532e83ef2b1341e50ecb69febb54f8f8
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-11-16 14:14:16 +0000

    added tutorial coordinates

--- text-game/Program.cs
+++ text-game/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Xml.Linq;
-
-namespace text_game;
+﻿namespace text_game;
 
 internal class Program
 {
@@ -67,14 +65,57 @@ internal class Program
 
     static void Tutorial()
     {
+        while (true)
+        {
+            DisplayPlayerInformation();
+
+            Helpers.ColouredText("\n\nYou decide to look around the room.", ConsoleColor.White);
+            Helpers.ColouredText("\n\nTry typing \'u\' to move up by 1 on \'y\'\n", ConsoleColor.White);
+
+            string? input = Console.ReadLine();
+
+            if (input != "u")
+            {
+                Helpers.DisplayError("Please enter \'u\'.");
+                Helpers.DisplayEnterPrompt();
+
+                continue;
+            }
+            UpdateCoordinate(input);
+            break;
+        }
+
+
+
+        while (true)
+        {
+            DisplayPlayerInformation();
+            Console.WriteLine("\n\nThis means you switched the tile your character is currently on.\nYou will encounter various things on each tile.\n\n");
+
+            Console.WriteLine("Try moving to another tile:\n" +
+                "\n\t'u' (up)     = y + 1" +
+                "\n\t'd' (down)   = y - 1" +
+                "\n\t'l' (left)   = x - 1" +
+                "\n\t'r' (right)  = x + 1");
+
+            string? input = Console.ReadLine();
+
+            if (input != "u" && input != "d" && input != "l" && input != "r")
+            {
+                Helpers.DisplayError("Please enter a valid coordinate character.");
+                Helpers.DisplayEnterPrompt();
+
+                continue;
+            }
+            UpdateCoordinate(input);
+            break;
+        }
+
         DisplayPlayerInformation();
-
-        Helpers.TypeColouredText("\n\nYou decide to look around the room.", ConsoleColor.White, 50);
-        Helpers.TypeColouredText("\n\nTry typing \'u\' to move up by 1 on \'x\'", ConsoleColor.White, 50);
-
+        Console.WriteLine("\n\nHere you can see your location has updated again.\nTry exploring the rest of the laboratory.");
+        Helpers.DisplayEnterPrompt();
     }
-
-    static void DisplayPlayerInformation() 
+    static void DisplayPlayerInformation()
     {
         Console.Write($"Name:");
         Helpers.ColouredText($" {character.name}", ConsoleColor.Green);
@@ -85,26 +126,35 @@ internal class Program
         Console.WriteLine($"\n\nCurrent Location: x {x}, y {y}\n__________________________");
     }
 
-    static void UpdateCoordinate(string coordinate) 
+    static void UpdateCoordinate(string coordinate)
     {
-        switch (coordinate) 
+        switch (coordinate)
         {
-            case "u": y += 1;
+            case "u":
+                y += 1;
+                Helpers.ColouredText($"\nYou moved from y {y - 1} to y {y}", ConsoleColor.Yellow);
                 break;
 
-            case "d": y -= 1;
+            case "d":
+                y -= 1;
+                Helpers.ColouredText($"\nYou moved from y {y + 1} to y {y}", ConsoleColor.Yellow);
                 break;
 
-            case "l": x -= 1;
+            case "l":
+                x -= 1;
+                Helpers.ColouredText($"\nYou moved from x {x + 1} to x {x}", ConsoleColor.Yellow);
                 break;
 
-            case "r": x += 1;
+            case "r":
+                x += 1;
+                Helpers.ColouredText($"\nYou moved from x {x - 1} to x {x}", ConsoleColor.Yellow);
                 break;
         }
+        Helpers.DisplayEnterPrompt();
     }
 
-    static void Game() 
-    { 
-    
+    static void Game()
+    {
+
     }
 }
